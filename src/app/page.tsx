7deee611ce80eba49b7772ee1deb4564279941df
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-02-11 02:58:31 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className='flex justify-center items-center h-screen flex-1 font-aeronik'>
+      <Link href={'/dashboard/partners'}>
+      <button className='p-2 rounded-md border-[1px]'>Go to dashboard</button>
+      </Link>
+    </div>
   )
 }
 
