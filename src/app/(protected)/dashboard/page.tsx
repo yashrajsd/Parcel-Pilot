6dc9b88e527c734c233c962c42b6184ecf09eecf
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-02-11 11:10:49 +0530

    minor changes

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+'use client'
+import { Loader } from 'lucide-react';
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const Page = () => {
+  const router = useRouter();
+  useEffect(()=>{
+    router.push('/dashboard/partners')
+  },[])
   return (
-    <div>
-        
+    <div className='flex flex-1 h-screen justify-center items-center'>
+        <Loader className='animate-spin'/>
     </div>
   )
 }
